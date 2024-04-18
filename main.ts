97f7448a3f97eab6e61f7e0d78095f00a1fba5ce
Rabia Mashkoor <rabiamashkoor758@gmail.com>
2024-04-17 18:11:30 -0700

    This is my simple todo list

---
+++ main.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blueBright("Welcome to my todo list"));
+
+let todos = []
+let condition = true;
+
+while (condition)
+{
+    let addtask = await inquirer.prompt(
+        [
+           {
+               name: "task",
+               type: "input",
+               message: "What you want to add in your task?"
+           },
+           {
+               name: "addmore",
+               type: "confirm",
+               message: "Do you want to add more task?",
+               default: true
+            }
+        ]
+    )
+    todos.push(addtask.task);
+    condition = addtask.addmore;
+    console.log(todos);
+}
